0b314b9c933b0f20fe9f35d5ecce027eb1123d89
becevka <becevka@gmail.com>
2013-08-06 19:07:15 +0300

    Focused on cli version

---
+++ src/main/java/net/kucoe/elvn/sync/ConsoleSyncStatus.java
@@ -0,0 +1,16 @@
+package net.kucoe.elvn.sync;
+
+
+/**
+ * Console implementation for {@link SyncStatusListener}
+ * 
+ * @author Vitaliy Basyuk
+ */
+public class ConsoleSyncStatus implements SyncStatusListener {
+    
+    @Override
+    public void onStatusChange(final String status) {
+        System.out.println(status);
+    }
+    
+}
