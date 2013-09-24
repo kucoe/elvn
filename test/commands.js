7ffda4a5af82b7731a9cdfd4b5c7baa1a2adf448
becevka <becevka@gmail.com>
2013-09-24 21:49:16 +0300

    Commands implementation

--- test/commands.js
+++ test/commands.js
@@ -1,46 +1,50 @@
 var cli = require('cline')();
 var should = require('should');
-var fs = require('fs');
-var results = require('../lib/results');
+var commands = require('../lib/commands');
 
-require('../lib/commands').init(cli);
+commands.init(cli);
+
+
+cli.on('command', function (main) {
+    //console.log(main);
+});
 
 describe('commands', function () {
     it('should parse list switch', function () {
         var parse = cli.parse('/all');
-        parse.should.be.instanceof(results.SwitchList, 'list switch result');
+        parse.should.be.instanceof(commands.SwitchList, 'list switch command');
         parse.color.should.eql('all', 'list color ');
     });
     it('should parse list editor switch', function () {
         var parse = cli.parse('&');
-        parse.should.be.instanceof(results.SwitchListEdit, 'list edit switch result');
+        parse.should.be.instanceof(commands.SwitchListEdit, 'list edit switch command');
     });
     it('should parse ideas switch', function () {
         var parse = cli.parse('@');
-        parse.should.be.instanceof(results.SwitchIdeas, 'ideas switch result');
+        parse.should.be.instanceof(commands.SwitchIdeas, 'ideas switch command');
     });
     it('should parse status switch', function () {
         var parse = cli.parse('!');
-        parse.should.be.instanceof(results.SwitchStatus, 'status switch result');
+        parse.should.be.instanceof(commands.SwitchStatus, 'status switch command');
     });
     it('should parse sync switch', function () {
         var parse = cli.parse('%');
-        parse.should.be.instanceof(results.SwitchSync, 'sync switch result');
+        parse.should.be.instanceof(commands.SwitchSync, 'sync switch command');
         parse.command.should.eql('', 'sync command ');
     });
     it('should parse sync command', function () {
         var parse = cli.parse('%>');
-        parse.should.be.instanceof(results.SwitchSync, 'sync switch result');
+        parse.should.be.instanceof(commands.SwitchSync, 'sync switch command');
         parse.command.should.eql('>', 'sync command ');
     });
     it('should parse timer switch', function () {
         var parse = cli.parse('$');
-        parse.should.be.instanceof(results.SwitchTimer, 'timer switch result');
+        parse.should.be.instanceof(commands.SwitchTimer, 'timer switch command');
         parse.command.should.eql('', 'timer command ');
     });
     it('should parse timer command', function () {
         var parse = cli.parse('$:');
-        parse.should.be.instanceof(results.SwitchTimer, 'timer switch result');
+        parse.should.be.instanceof(commands.SwitchTimer, 'timer switch command');
         parse.command.should.eql(':', 'timer command ');
     });
     it('should parse wrong list editor switch', function () {
@@ -61,6 +65,111 @@ describe('commands', function () {
     });
     it('should parse wrong timer switch', function () {
         var parse = cli.parse('$s');
+        should.not.exist(parse);
+    });
+    it('should parse create task', function () {
+        var parse = cli.parse('g:text');
+        parse.should.be.instanceof(commands.EditTask, 'task command');
+        parse.color.should.eql('g', 'color');
+        parse.text.should.eql('text', 'text');
+    });
+    it('should parse create simple task', function () {
+        var parse = cli.parse(':text');
+        parse.should.be.instanceof(commands.EditTask, 'task command');
+        parse.color.should.eql('', 'color');
+        parse.text.should.eql('text', 'text');
+    });
+    it('should parse search task', function () {
+        var parse = cli.parse('?alla');
+        parse.should.be.instanceof(commands.SearchTask, 'task search command');
+        parse.color.should.eql('', 'color');
+        parse.text.should.eql('', 'text');
+        parse.query.should.eql('alla', 'query');
+    });
+    it('should parse search task with replace', function () {
+        var parse = cli.parse('?correct=correct%fixed');
+        parse.should.be.instanceof(commands.SearchTask, 'task search command');
+        parse.color.should.eql('', 'color');
+        parse.text.should.eql('correct%fixed', 'text');
+        parse.query.should.eql('correct', 'query');
+    });
+    it('should parse search task with replace shortcut', function () {
+        var parse = cli.parse('?correct%fixed');
+        parse.should.be.instanceof(commands.SearchTask, 'task search command');
+        parse.color.should.eql('', 'color');
+        parse.text.should.eql('correct%fixed', 'text');
+        parse.query.should.eql('correct', 'query');
+    });
+    it('should parse locate task', function () {
+        var parse = cli.parse('#2');
+        parse.should.be.instanceof(commands.LocateTask, 'task locate command');
+        parse.color.should.eql('', 'color');
+        parse.text.should .eql('', 'text');
+        parse.command.should.eql('', 'command');
+        parse.positions.should.eql([2], 'positions');
+    });
+    it('should parse locate task with command', function () {
+        var parse = cli.parse('#2-');
+        parse.should.be.instanceof(commands.LocateTask, 'task locate command');
+        parse.color.should.eql('', 'color');
+        parse.text.should .eql('', 'text');
+        parse.command.should.eql('-', 'command');
+        parse.positions.should.eql([2], 'positions');
+    });
+    it('should parse locate range task', function () {
+        var parse = cli.parse('#2-4');
+        parse.should.be.instanceof(commands.LocateTask, 'task locate command');
+        parse.color.should.eql('', 'color');
+        parse.text.should .eql('', 'text');
+        parse.command.should.eql('', 'command');
+        parse.positions.should.eql([2,3,4], 'positions');
+    });
+    it('should parse locate reverse range task', function () {
+        var parse = cli.parse('#4-2');
+        parse.should.be.instanceof(commands.LocateTask, 'task locate command');
+        parse.color.should.eql('', 'color');
+        parse.text.should .eql('', 'text');
+        parse.command.should.eql('', 'command');
+        parse.positions.should.eql([2,3,4], 'positions');
+    });
+    it('should parse locate big range task', function () {
+        var parse = cli.parse('#2-225');
+        parse.should.be.instanceof(commands.LocateTask, 'task locate command');
+        parse.color.should.eql('', 'color');
+        parse.text.should .eql('', 'text');
+        parse.command.should.eql('', 'command');
+        var arr = [];
+        for(var i = 2; i <= 225; i++) {
+            arr.push(i);
+        }
+        parse.positions.should.eql(arr, 'positions');
+    });
+    it('should parse locate range task with command', function () {
+        var parse = cli.parse('#2-4>');
+        parse.should.be.instanceof(commands.LocateTask, 'task locate command');
+        parse.color.should.eql('', 'color');
+        parse.text.should .eql('', 'text');
+        parse.positions.should.eql([2,3,4], 'positions');
+        parse.command.should.eql('>', 'command');
+    });
+    it('should parse locate group task', function () {
+        var parse = cli.parse('#2,4');
+        parse.should.be.instanceof(commands.LocateTask, 'task locate command');
+        parse.color.should.eql('', 'color');
+        parse.text.should .eql('', 'text');
+        parse.command.should.eql('', 'command');
+        parse.positions.should.eql([2,4], 'positions');
+    });
+    it('should parse locate group task with command', function () {
+        var parse = cli.parse('#2,4-');
+        parse.should.be.instanceof(commands.LocateTask, 'task locate command');
+        parse.color.should.eql('', 'color');
+        parse.text.should .eql('', 'text');
+        parse.command.should.eql('-', 'command');
+        parse.positions.should.eql([2,4], 'positions');
+    });
+    it('should parse wrong locate', function () {
+        var parse = cli.parse('#s');
         should.not.exist(parse);
     });
 });
