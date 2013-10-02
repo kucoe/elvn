44b26e29e263b2c5e8bae9f0c6fa34bb16722f72
becevka <becevka@gmail.com>
2013-10-02 21:25:44 +0300

    Config, Display, Colors

--- test/config.js
+++ test/config.js
@@ -1,5 +1,6 @@
 var should = require('should');
 var commands = require('../lib/commands');
+var fs = require('fs');
 
 var d = require('../lib/display');
 var c = require('../lib/config');
@@ -9,13 +10,21 @@ var config = new c.Config();
 
 display.currentList = 'all';
 
-config.getBasePath = function() {
+config.getBasePath = function () {
     return this.getUserDir() + "/.elvn-test/";
 };
 
 describe('config', function () {
+    afterEach(function () {
+        var basePath = config.getBasePath();
+        var files = fs.readdirSync(basePath);
+        files.forEach(function (file, index) {
+            fs.unlinkSync(basePath + "/" + file);
+        });
+        fs.rmdirSync(basePath);
+    });
     it('should create task', function () {
-        var size = config.getList('a').tasks.length;
+        var size = config.getList('all').tasks.length;
         var command = new commands.EditTask("b", "a");
         command.run(display, config).should.eql('all', 'current list');
         config.getList('a').tasks.length.should.eql(size + 1, 'list size');
