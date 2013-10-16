627a459088f1dc1ef82837d0aeeca366242d39c3
becevka <becevka@gmail.com>
2013-10-16 18:47:10 +0300

    Sync ready

--- test/sync.js
+++ test/sync.js
@@ -5,15 +5,16 @@ var c = require('../lib/config');
 var s = require('../lib/sync');
 
 
-var config, sync, syncNoKey;
-var cli = {
-    password: function () {
-        return 'aaa';
-    }
-};
+var config, sync, syncNoKey, cli;
 
-describe.only('sync', function () {
+
+describe('sync', function () {
     beforeEach(function () {
+        cli = {
+            password: function (text, cb) {
+                cb && cb('aaa');
+            }
+        };
         config = new c.Config();
         config.getBasePath = function () {
             return this.getUserDir() + "/.elvn-test/";
@@ -30,11 +31,32 @@ describe.only('sync', function () {
         });
         fs.rmdirSync(basePath);
     });
-    it('should auth', function () {
+    it('should auth', function (done) {
         sync.pull();
         setTimeout(function () {
             fs.existsSync(config.getBasePath() + "sync.key").should.eql(true, 'key exists');
             sync.authorized.should.eql(true, 'authorized');
+            done();
+        }, 1000);
+    });
+    it('should auth fail on wrong pass', function (done) {
+        cli.password = function (text, cb) {
+            cb && cb('bbb');
+        };
+        sync.pull();
+        this.timeout(5000);
+        setTimeout(function () {
+            fs.existsSync(config.getBasePath() + "sync.key").should.eql(false, 'key not exists');
+            sync.authorized.should.eql(false, 'not authorized');
+            done();
+        }, 3000);
+    });
+    it('should support nokey', function (done) {
+        syncNoKey.pull();
+        setTimeout(function () {
+            fs.existsSync(config.getBasePath() + "sync.key").should.eql(false, 'key not exists');
+            syncNoKey.authorized.should.eql(true, 'authorized');
+            done();
         }, 1000);
     });
 });
